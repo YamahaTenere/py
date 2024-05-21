7b5669b4825930a09984f2dc8cb7b280830cbbe6
Zeit <yamahatenere80@gmail.com>
2024-05-21 13:17:03 +0300

    111

---
+++ first/nums.py
@@ -0,0 +1,16 @@
+def plus(a, b):
+    return a + b
+
+
+text = 'asgfdsgdsf'
+
+
+def minus(x, y):
+    return x - y
+
+
+if __name__ == '__main__':
+    print(plus(2, 2))
+    print(plus(2, 2))
+    print(plus(2, 2))
+    print(minus(10,5))
